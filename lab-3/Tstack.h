aad94632b135b1afb7d5b450cf446e568be188d9
unknown <rodimkov.ju@WS-6K-102-A09.ccam.cmc.unn.net>
2017-10-25 10:32:39 +0300

    qwe

--- lab-3/Tstack.h
+++ lab-3/Tstack.h
@@ -1,11 +1,97 @@
-
 template <class T>
 
 class Tstack
 {
-
+private:
+	T *array;
+	int size;
+	int Max;
 public:
-	Tstack(){}
-
-
+	Tstack(int _size = 100);
+	~Tstack()	{ delete[] array; };
+	Tstack(const Tstack& ts);
+	Tstack& operator=(const Tstack& ts);
+	int isfull();
+	int isempty();
+	T top();
+	T pop();
+	void push(T el);
 };
+
+template <class T>
+Tstack<T>::Tstack(int _size)
+{
+	if( _size <= 0) throw _size;
+	array = new T[size];
+	Max = _size;
+	size = 0;
+}
+
+template <class T>
+Tstack<T>::Tstack(const Tstack& ts)
+{
+	size = ts.size;
+	Max = ts.Max;
+	for(int i = 0 ; i< size ; i++) 
+	{
+		array[i] = ts.array[i];
+	}
+}
+
+template <class T>
+Tstack<T>& Tstack<T>::operator=(const Tstack& ts)
+{
+	if( this == &ts ) return this;
+		if( Max != ts.Max )
+		{
+			delete[] array ;
+			Max = ts.Max;
+			array = new T[Max];
+		}
+		size = ts.size;
+		for( int i = 0 ; i <size ; i++)
+			array[i] = ts.array[i];
+	return *this;
+}
+
+template <class T>
+int Tstack<T>::isfull()
+{
+	if(size == Max)
+		return 1;
+	else
+		return 0;
+}
+
+template <class T>
+int Tstack<T>::isempty()
+{
+	if(size == 0)
+		return 1;
+	else
+		return 0;
+}
+
+template <class T>
+T Tstack<T>::top()
+{
+	if(isempty) throw -1;
+	return array[size-1];
+}
+
+
+template <class T>
+T Tstack<T>::pop()
+{
+	if(isempty) throw -1;
+	size--;
+	return array[size];
+}
+
+template <class T>
+void Tstack<T>::push(T el)
+{
+	if(isfull) throw -1;
+	array[size] = el;
+	size++;
+}
